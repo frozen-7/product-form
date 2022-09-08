18591a21c0e8cae2d350a817cf2a3bed89a91605
Insharah Ayyubi <weharrypotter@gmail.com>
2022-09-08 23:45:39 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const input = document.querySelector(".input-number");
+const plus = document.getElementById("plus");
+const minus = document.querySelector(".minus");
+plus.onClick= function ()
+{
+    console.log( "input.innerHTML");
+};
+ 
