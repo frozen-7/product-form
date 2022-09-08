15b72943d1c18029e0bd64188a837866ce311b82
Insharah Ayyubi <103804673+frozen-7@users.noreply.github.com>
2022-09-08 23:48:00 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const input = document.querySelector(".input-number");
-const plus = document.getElementById("plus");
-const minus = document.querySelector(".minus");
-plus.onClick= function ()
-{
-    console.log( "input.innerHTML");
-};
- 
